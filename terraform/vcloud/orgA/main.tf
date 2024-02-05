264a9a3cfa8f927356e5b844032eb3c7de7cbefc
enjangse88 <enjang.setiawan88@gmail.com>
2024-02-05 13:24:16 +0700

    terraform vcloud

---
+++ terraform/vcloud/orgA/main.tf
@@ -0,0 +1,26 @@
+provider "vcd" {
+  user     = var.admin_user
+  password = var.admin_password
+  org      = "System"
+  url      = "https://AcmeVcd/api"
+}
+
+resource "vcd_org" "my-org" {
+  name             = "my-org"
+  full_name        = "My organization"
+  description      = "The pride of my work"
+  is_enabled       = true
+  delete_recursive = true
+  delete_force     = true
+
+  vapp_lease {
+    maximum_runtime_lease_in_sec          = 3600 # 1 hour
+    power_off_on_runtime_lease_expiration = true
+    maximum_storage_lease_in_sec          = 0 # never expires
+    delete_on_storage_lease_expiration    = false
+  }
+  vapp_template_lease {
+    maximum_storage_lease_in_sec       = 604800 # 1 week
+    delete_on_storage_lease_expiration = true
+  }
+}
